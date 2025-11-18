799301f18c8757ad529ac4b2e36e5b54ee11a4b2
Pavishni <pavishni.r@gmail.com>
2025-11-18 08:49:50 +0530

    exploring middlewares

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const {auth} = require("./middlewares/auth")
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("Hello world");
+app.use("/admin", auth);
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Data successfully fetched");
 });
 
-app.post("/user", (req, res)=>{
-  res.send("Data successfully inserted");
-});
-
-app.delete("/user",(req,res)=>{
-  res.send("Data successfully deleted");
+app.get("/admin/deleteData", (req, res) => {
+  res.send("Data deleted successfully");
 });
 
 app.listen(7777, () => {
---
+++ src/middlewares/auth.js
@@ -0,0 +1,13 @@
+const auth = (req, res, next) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  auth,
+};
