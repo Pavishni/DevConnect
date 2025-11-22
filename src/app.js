a0c78143f65ae3bff4531058b606713e92254b8d
Pavishni <pavishni.r@gmail.com>
2025-11-22 07:25:21 +0530

    database init, create APIs in express and CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,18 +1,57 @@
 const express = require("express");
-const {auth} = require("./middlewares/auth")
-
+const { auth } = require("./middlewares/auth");
+const connectDb = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-app.use("/admin", auth);
+app.use(express.json());
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("Data successfully fetched");
+app.post("/signUp", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("Data successfully inserted");
+  } catch (err) {
+    res.status(400).send("Error saving", err.message);
+  }
 });
 
-app.get("/admin/deleteData", (req, res) => {
-  res.send("Data deleted successfully");
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
-app.listen(7777, () => {
-  console.log("Server running successfully in port http://localhost:7777/....");
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
+
+connectDb()
+  .then(() => {
+    console.log("Database connected successfully...");
+    app.listen(7777, () => {
+      console.log(
+        "Server running successfully in port http://localhost:7777/...."
+      );
+    });
+  })
+  .catch((err) => {
+    console.error("Database not connected");
+  });
