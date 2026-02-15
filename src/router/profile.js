4427a5d3b9709cad4436266cd79e31bff84b2878
Pavishni <pavishni.r@gmail.com>
2026-02-15 20:54:05 +0530

    password update api

--- src/router/profile.js
+++ src/router/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateProfileEditData } = require("../utils/validations");
 const profileRouter = express.Router();
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -19,19 +20,24 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     }
     const loggedInUser = req.user;
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
-    loggedInUser.save();
-    res.send("Edit was saved successfully");
+    await loggedInUser.save();
+    res.send( `Hi ${loggedInUser.firstName} your data was saved successfully`);
   } catch (err) {
     res.status(400).send("Error :" + err.message);
   }
 });
 
-// profileRouter.patch("/profile/password", userAuth, async (req, res) => {
-//   try {
-
-//   } catch (err) {
-//     res.status(400).send("Error :" + err.message);
-//   }
-// });
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { password } = req.body;
+    const user = req.user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).send("Password updated successfully");
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
 
 module.exports = profileRouter;
